d8f5c6194e3b4f0fd6ee8537f62f6a01e7fce5e2
Орлов Алексей <A.Orlov@hostco.ru>
2023-09-08 11:45:38 +0500

    добавить статусы из Redmine

---
+++ src/IssueTrackerSynchronizationService.Dto/Enums/RedmineIssueStatuses.cs
@@ -0,0 +1,33 @@
+﻿namespace IssueTrackerSynchronizationService.Dto.Enums
+{
+    /// <summary>
+    /// Статусы задач в Redmine
+    /// </summary>
+    public enum RedmineIssueStatuses
+    {
+        /// <summary>
+        /// Закрыт
+        /// </summary>
+        Closed = 5,
+
+        /// <summary>
+        /// В работе
+        /// </summary>
+        InWork = 10,
+
+        /// <summary>
+        /// Тестирование
+        /// </summary>
+        Testing = 11,
+
+        /// <summary>
+        /// Отладка
+        /// </summary>
+        Debugging = 12,
+
+        /// <summary>
+        /// Code Review
+        /// </summary>
+        CodeReview = 15
+    }
+}
